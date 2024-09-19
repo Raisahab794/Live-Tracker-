94be284863446e41b6ff923d4125677e990db20e
Ayush <ayushkumarrai2@gmail.com>
2024-09-19 20:16:42 +0530

    new icon

--- app.js
+++ app.js
@@ -21,4 +21,7 @@ io.on('connection',function(socket){
 app.get('/',function(req,res){
     res.render('index');
 })
-server.listen(3000);
+const PORT=process.env.PORT || 3000;
+server.listen(PORT,function(){
+    console.log(`Server started on port ${PORT}`);
+})
--- public/js/script.js
+++ public/js/script.js
@@ -1,60 +1,59 @@
-const socket=io();
+const socket = io();
 
-
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition(position=>{
-        const {latitude,longitude}=position.coords;
-        socket.emit('sendLocation',{latitude,longitude});
-    },
-           (error)=>{ 
-            console.log(error);
-              },
-              {
-                enableHighAccuracy:true,
-                timeout:5000,
-                maximumAge:0
-                }
-)
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        position => {
+            const { latitude, longitude } = position.coords;
+            console.log(`Sending location: ${latitude}, ${longitude}`); 
+            socket.emit('sendLocation', { latitude, longitude });
+        },
+        error => {
+            console.log(`Geolocation error: ${error.message}`);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        }
+    );
+} else {
+    console.log('Geolocation is not supported by this browser.');
 }
 
-const map = L.map('map').setView([0, 0], 13);
+const map = L.map('map').setView([0, 0], 2); 
 
-// Add a tile layer to the map (OpenStreetMap tiles)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Add a marker to the map
-const marker={};
+const personIcon = L.divIcon({
+    html: '👤',
+    className: 'custom-icon',
+    iconSize: [30, 30],
+    iconAnchor: [15, 30]
+});
 
-socket.on('receiveLocation',function(data){
-    const {latitude,longitude,socketId}=data;
-   map.setView([latitude,longitude]);
-    if(marker[socketId]){
-        marker[socketId].setLatLng([latitude,longitude]);
-    }else{
-        marker[socketId]=L.marker([latitude,longitude]).addTo(map);
+const markers = {};
+
+socket.on('receiveLocation', function(data) {
+    const { latitude, longitude, socketId } = data;
+    console.log(`Received location: ${latitude}, ${longitude} from ${socketId}`); 
+    map.setView([latitude, longitude], 13); 
+    if (markers[socketId]) {
+        markers[socketId].setLatLng([latitude, longitude]);
+    } else {
+        markers[socketId] = L.marker([latitude, longitude], { icon: personIcon }).addTo(map);
     }
-})
+});
 
-// Add a circle to the map
- const circle = L.circle([0, 0], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 50
-    }).addTo(map);
+const circle = L.circle([0, 0], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
 
-    socket.on('receiveLocation', function(data) {
-        const { latitude, longitude } = data;
-        circle.setLatLng([latitude, longitude]);
-    });
-      socket.on('user-disconnect',function(data){
-        const {socketId}=data;
-        if(marker[socketId]){
-            map.removeLayer(marker[socketId]);
-            delete marker[socketId];
-        }
-
-    }
-    )
+socket.on('receiveLocation', function(data) {
+    const { latitude, longitude } = data;
+    circle.setLatLng([latitude, longitude]);
+});
